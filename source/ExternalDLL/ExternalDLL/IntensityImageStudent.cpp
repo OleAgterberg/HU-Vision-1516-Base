fafd1369b2204fb3345ae2b82de7a2d6ee9ebb6f
OleAgterberg <oleagterberg@gmail.com>
2016-04-20 11:22:11 +0200

    cleaar

--- source/ExternalDLL/ExternalDLL/IntensityImageStudent.cpp
+++ source/ExternalDLL/ExternalDLL/IntensityImageStudent.cpp
@@ -5,60 +5,37 @@ IntensityImageStudent::IntensityImageStudent() : IntensityImage() {
 }
 
 IntensityImageStudent::IntensityImageStudent(const IntensityImageStudent &other) : IntensityImage(other.getWidth(), other.getHeight()) {
-    int width = getWidth(), height = getHeight();
-    image.clear();
-    //image_pointer = image.get_allocator().allocate(width*height);
-    for (int y = 0; y < height; y++){
-        for (int x = 0; x < width; x++){
-            setPixel(x, y, other.getPixel(x, y));
-        }
-    }
+
     //TODO: Create a copy from the other object
 }
 
 IntensityImageStudent::IntensityImageStudent(const int width, const int height) : IntensityImage(width, height) {
-    //image_pointer = image.get_allocator().allocate(width*height);
-    image.clear();
+
     //TODO: Initialize pixel storage
 }
 
 IntensityImageStudent::~IntensityImageStudent() {
-    //image.get_allocator().deallocate(image_pointer, getWidth() * getHeight());
-    image.clear();
+
 	//TODO: delete allocated objects
 }
 
 void IntensityImageStudent::set(const int width, const int height) {
-    int oldHeight = getHeight();
-    int oldWidth = getWidth();
-    IntensityImage::set(width, height);
-    image.clear();
-    //image.get_allocator().deallocate(image_pointer, oldHeight*oldWidth);
-    //image_pointer = image.get_allocator().allocate(width*height);
+
     //TODO: resize or create a new pixel storage (Don't forget to delete the old storage)
 }
 
 void IntensityImageStudent::set(const IntensityImageStudent &other) {
-    int height = other.getHeight(), width = other.getWidth();
-    IntensityImage::set(width, height);
 
-    image.clear();
-    for (int y = 0; y < height; y++){
-        for (int x = 0; x < width; x++){
-            setPixel(x, y, other.getPixel(x, y));
-        }
-    }
     //TODO: resize or create a new pixel storage and copy the object (Don't forget to delete the old storage)
 }
 
 void IntensityImageStudent::setPixel(int x, int y, Intensity pixel) {
-    image[y][x] = pixel;
+
     //TODO: no comment needed :)
 }
 
 void IntensityImageStudent::setPixel(int i, Intensity pixel) {
-    int x = i % getWidth(), y = i / getWidth();
-    image[y][x] = pixel;
+
 	/*
 	* TODO: set pixel i in "Row-Major Order"
 	*
@@ -83,16 +60,11 @@ void IntensityImageStudent::setPixel(int i, Intensity pixel) {
 }
 
 Intensity IntensityImageStudent::getPixel(int x, int y) const {
-    std::map<const int, std::map<int, Intensity, std::less<int>, std::allocator<std::pair<const int, Intensity>>>>::const_iterator it_x;
-    std::map<int, Intensity>::const_iterator it_y;
-    it_x = image.find(x);
-    it_y = it_x->second.find(y);
-    return it_y->second;
+
 	//TODO: no comment needed :)
 }
 
 Intensity IntensityImageStudent::getPixel(int i) const {
-    int x = i % getWidth(), y = i / getWidth();
-    return getPixel(x, y);
+
 	//TODO: see setPixel(int i, RGB pixel)
 }
--- source/ExternalDLL/ExternalDLL/RGBImageStudent.cpp
+++ source/ExternalDLL/ExternalDLL/RGBImageStudent.cpp
@@ -7,16 +7,7 @@ RGBImageStudent::RGBImageStudent() : RGBImage(), ARRAYSIZE{ 0 }{
 }
 
 RGBImageStudent::RGBImageStudent(const RGBImageStudent &other) : RGBImage(other.getWidth(), other.getHeight()), ARRAYSIZE{ other.getWidth() * other.getHeight() }{
-    int width = getWidth(), height = getHeight();
-    image.clear();
-    //image_pointer = image.get_allocator().allocate(width*height);
-    for (int y = 0; y < height; y++){
-        for (int x = 0; x < width; x++){
-            setPixel(x, y, other.getPixel(x, y));
-            //RGB pixel = other.getPixel(x, y);
-            //std::cout << "(" << pixel.b << ", " << pixel.g << ", " << pixel.r << ") ";
-        }
-    }
+    
 	//TODO: Create a copy from the other object
 	// Copy rgbimage contents
 	// Ferdi Method
@@ -24,70 +15,33 @@ RGBImageStudent::RGBImageStudent(const RGBImageStudent &other) : RGBImage(other.
 }
 
 RGBImageStudent::RGBImageStudent(const int width, const int height) : RGBImage(width, height), ARRAYSIZE{ width * height } {
-    //int throwError = 0, e = 1 / throwError;
-    image.clear();
+    
 	//TODO: Initialize pixel storage
 
 	// Ferdi Method
-	initVector(width, height);
 }
 
 RGBImageStudent::~RGBImageStudent() {
-    //image.get_allocator().deallocate(image_pointer, getWidth() * getHeight());
-    image.clear();
 	//TODO: delete allocated objects
 }
 
 void RGBImageStudent::set(const int width, const int height) {
 
 	//TODO: resize or create a new pixel storage (Don't forget to delete the old storage)
-	RGBImage::set(width, height);
-	int throwError = 0, e = 1 / throwError;
-	
-	RGB **vi_array_t;
-	vi_array_t = new RGB*[width];
-	for (size_t i = 0; i < width; i++)
-	{
-		vi_array_t[i] = new RGB[height]{ RGB(0, 0, 0) };
-	}
-	memcpy(vi_array_t, vi_array, (width * height * sizeof(RGB))); // should be done for each row!
 }
 
 void RGBImageStudent::set(const RGBImageStudent &other) {
-    int height = other.getHeight(), width = other.getWidth();
-	RGBImage::set(width, height);
-
-    image.clear();
-    for (int y = 0; y < height; y++){
-        for (int x = 0; x < width; x++){
-            setPixel(x, y, other.getPixel(x, y));
-        }
-    }
-
-    //////////////////////////////////////
-
 	//TODO: resize or create a new pixel storage and copy the object (Don't forget to delete the old storage)
-	emptyArray();	// Deleting old data
-	RGBImage::set(other.getWidth(), other.getHeight());	// setting RGB Image width and height
-	copy_vimage(other);	// Copying other RBGImageStudent content to this RGBImageStudent
 	//int throwError = 0, e = 1 / throwError; This method has been implemented
 	
 }
 
 void RGBImageStudent::setPixel(int x, int y, RGB pixel) {
-    image[y][x] = pixel;
-    //image_pointer = image.get_allocator().allocate(width*height);
-    /*std::cout << "(" << int(pixel.b) << ", " << int(pixel.g) << ", " << int(pixel.r) << ") ";
-    if (getWidth() == x){
-        std::cout << std::endl;
-    }*/
     //TODO: Create a copy from the other 
 	//TODO: no comment needed :)
 }
 
 void RGBImageStudent::setPixel(int i, RGB pixel) {
-    int x = 1, y = 1; //i % getWidth(), y = i / getWidth();
-    image[y][x] = pixel;
 	/*
 	* TODO: set pixel i in "Row-Major Order"
 	*
@@ -113,45 +67,9 @@ void RGBImageStudent::setPixel(int i, RGB pixel) {
 
 
 RGB RGBImageStudent::getPixel(int x, int y) const {
-    std::map<const int, std::map<int, RGB, std::less<int>, std::allocator<std::pair<const int, RGB>>>>::const_iterator it_x;
-    std::map<int, RGB>::const_iterator it_y;
-    it_x = image.find(x);
-    it_y = it_x->second.find(y);
-    return it_y->second;
 	//TODO: no comment needed :)
 }
 
 RGB RGBImageStudent::getPixel(int i) const {
-    int x = i % getWidth(), y = i / getWidth();
-    RGB pixel = getPixel(x, y);
-    return getPixel(x, y);
 	//TODO: see setPixel(int i, RGB pixel)
-}
-void RGBImageStudent::copy_vimage(const RGBImageStudent &other)	{
-	// Delete old data
-	vi_array = new RGB*[other.width];
-	for (size_t i = 0; i < other.width; i++)
-	{
-		vi_array[i] = new RGB[other.height]{ RGB(0, 0, 0) };
-		for (size_t j = 0; j < other.height; j++)
-		{
-			// Copy the other array data into the new array data
-			vi_array[i][j] = other.vi_array[i][j];
-		}
-	}
-}
-void RGBImageStudent::initVector(const int width, const int height)	{
-	vi_array = new RGB*[width];
-	for (size_t i = 0; i < width; i++)
-	{
-		vi_array[i] = new RGB[height]{ RGB(0, 0, 0) };
-	}
-}
-void RGBImageStudent::emptyArray()	{
-	// Cleanup multidimensional array
-	for (size_t i = 0; i < width; i++)
-	{
-		delete[] vi_array[i];
-	}
-	delete[] vi_array;
 }
